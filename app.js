d55d423189e256db51765ef540e0a570371fffdd
scunningham007 <73557464+scunningham007@users.noreply.github.com>
2021-01-13 13:26:08 +0000

    another  message to simon3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('sausages Hello World! - SIMON was the man... dough'));
+app.get('/', (req, res) => res.send('FLAPS Hello World! - SIMON was the man... dough'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
